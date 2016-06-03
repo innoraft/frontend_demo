6e3743c498787bf258a30a253705b1db77af3360
navdeep <navdeep.singh@innoraft.com>
2016-06-03 17:51:38 +0530

    front end demo work i#

---
+++ js/project.js
@@ -0,0 +1,23 @@
+alert("Welcome to the demo site");
+
+$(".new_click").click(function () {
+    if ($(".slide_div").css('right') === '-235px') {
+
+        $(".slide_div").animate({right: "0"});
+         $("body").animate({right: "235px"});
+
+    } else {
+        $(".slide_div").animate({
+            right: "-235px"
+            
+        });
+        $("body").animate({right: "0"});
+    }
+
+});
+$(".slide_div").blur(function () {
+            $(".slide_div").animate({
+                right: "-235px"
+            });
+    $("body").animate({right: "0"});
+      });
