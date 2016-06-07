c6226eec7278b429965d4faed3f09fa07cc3ed69
navdeep <navdeep.singh@innoraft.com>
2016-06-07 17:47:37 +0530

    new additions

--- js/project.js
+++ js/project.js
@@ -1,23 +1,29 @@
-alert("Welcome to the demo site");
-
 $(".new_click").click(function () {
     if ($(".slide_div").css('right') === '-235px') {
 
-        $(".slide_div").animate({right: "0"});
-         $("body").animate({right: "235px"});
+        $(".slide_div").animate({
+            right: "0"
+        });
+        $("body").animate({
+            right: "235px"
+        });
 
     } else {
         $(".slide_div").animate({
             right: "-235px"
-            
+
         });
-        $("body").animate({right: "0"});
+        $("body").animate({
+            right: "0"
+        });
     }
 
 });
 $(".slide_div").blur(function () {
-            $(".slide_div").animate({
-                right: "-235px"
-            });
-    $("body").animate({right: "0"});
-      });
+    $(".slide_div").animate({
+        right: "-235px"
+    });
+    $("body").animate({
+        right: "0"
+    });
+});
